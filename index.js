497296d2e832c118a60422919aac19f493990889
Mark Nosal <mark.c.nosal@gmail.com>
2023-01-12 20:07:30 -0600

    Finished Math lab

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+const num1 = Math.floor(2.5);
+const num5 = 12;
+const num6 = 19;
+const num2 = num5 + num6;
+const multiply = num1 * num2;
 
-// Write your code here
+const random = Math.floor(Math.random()) + 1;
+
+const num3 = 4;
+const num4 = 9;
+const mod = num3 % 9;
+
+const numberTwenty = (Math.floor(0.9999) + 2) * 10;
+
+const max = Math.max(num1, num3, num4, num5, num6, random, mod, numberTwenty);
